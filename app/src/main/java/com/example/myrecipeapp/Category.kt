ad2d8dfc7addc638146d93ff8b636a47daaf8c56
AlbertoRG <alberto.rg94@hotmail.com>
2024-07-12 13:17:02 -0600

    Day 10 of the Kotlin course, RecipeApp

--- app/src/main/java/com/example/myrecipeapp/Category.kt
+++ app/src/main/java/com/example/myrecipeapp/Category.kt
@@ -1,17 +1,14 @@
 package com.example.myrecipeapp
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Category(
     val idCategory: String,
     val strCategory: String,
     val strCategoryThumb: String,
     val strCategoryDescription: String
-)
+) : Parcelable
 
 data class CategoriesResponse(val categories: List<Category>)
-
-/*
-"idCategory": "1",
-"strCategory": "Beef",
-"strCategoryThumb": "https://www.themealdb.com/images/category/beef.png",
-"strCategoryDescription": "Beef is the culinary name for meat from cattle"
-*/
